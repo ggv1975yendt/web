16cd8a2afbceef1617ca62dc77645eb85372ba38
ggv1975yendt <118174718+ggv1975yendt@users.noreply.github.com>
2024-09-04 15:46:52 +0300

    Update pubtorr.js

--- pubtorr.js
+++ pubtorr.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
 
-    Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/pubtorr/pubtorr.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://ggv1975yendt.github.io/web/pubtorr/pubtorr.js'], function () { });
 
 })();
